a12625d8746a15c14754e26a66e8a2cfa737702e
Joh Yoshida <johyoshida@gmail.com>
2017-11-22 20:24:31 -0800

    Listener only exists on the last option

--- public/scripts/add-option.js
+++ public/scripts/add-option.js
@@ -1,16 +1,24 @@
 $(() => {
-  $(".option").on("focus", event => {
-    var created = false;
-    $(".option").on("keypress", event => {
-      console.log("here");
-      // var option = $(".option").val();
-      if (!created) {
-        $(".button").before($("<div class='input-group'></div>")
-        .append($("<label for=''>Option</label>"))
-        .append($("<input class='option form-control' type='text' name='option' value='' placeholder='option'>"))
-        .append($("<input class='option form-control' type='text' name='description' value='' placeholder='description'>")));
-        created = true;
-      }
+
+  function addInputGroup() {
+    $(".input-group").last().children(".option").on("focus", event => {
+      var created = false;
+      $(".option").on("keypress", event => {
+        var option = $(".option").val();
+        if (!option) {
+          if (!created) {
+            created = true;
+            $(".button").before($("<div class='input-group'></div>")
+            .append($("<label for=''>Option</label>"))
+            .append($("<input class='option form-control' type='text' name='option' value='' placeholder='option'>"))
+            .append($("<input class='option form-control' type='text' name='description' value='' placeholder='description'>")));
+            $(".option").off();
+            addInputGroup();
+          }
+        }
+      });
     });
-  });
+  }
+  addInputGroup()
+
 });
