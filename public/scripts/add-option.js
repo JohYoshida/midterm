50395f7689938053da13f620ae31fba19923a82b
Sebastian Bethell <sebbethell@gmail.com>
2017-11-25 02:26:52 +0000

    added margin to copyright on small screens

--- public/scripts/add-option.js
+++ public/scripts/add-option.js
@@ -12,13 +12,9 @@ $(() => {
             created = true;
             $(".button").before($("<div class='input-group'></div>")
             .append($(`<label for='option-${i}'>Option ${i}</label>`))
-<<<<<<< HEAD
-            .append($(`<input id="option-${i}" class='option form-control' type='text' name='option-${i}' value='' placeholder='Enter poll option'>`))
-            .append($("<input class='option form-control' type='text' name='description' value='' placeholder='Description'>")));
-=======
             .append($(`<input id="option-${i}" class='option form-control' type='text' name='option' value='' placeholder='Enter poll option'>`))
             .append($("<input class='option form-control' type='text' name='description' value='' placeholder='description'>")));
->>>>>>> dev
+
             $(".option").off();
             addInputGroup();
             i++
