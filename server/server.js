68e16a094116e06f7aa13af5cc23acc2798cc81e
Joh Yoshida <johyoshida@gmail.com>
2017-11-26 14:21:45 -0800

    Linting and converstion to ES6

--- server/server.js
+++ server/server.js
@@ -1,23 +1,21 @@
-"use strict";
+'use strict';
 
 require('dotenv').config();
 
 const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
+const ENV         = process.env.ENV || 'development';
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const sass        = require('node-sass-middleware');
 const app         = express();
 
-const knexConfig  = require("../knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
+const knexConfig  = require('../knexfile');
+const knex        = require('knex')(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 const mailgun     = require('../mailgun');
 
-
 const helpers = require('./lib/helpers.js');
-
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -27,16 +25,16 @@ app.use(morgan('dev'));
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/styles", sass({
-  src: "./styles",
-  dest:  "./public/styles",
+app.use('/styles', sass({
+  src: './styles',
+  dest: './public/styles',
   debug: true,
   outputStyle: 'expanded'
 }));
 console.log(__dirname);
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // handle favicon weirdness
 app.get('/favicon.ico', (req, res) => {
@@ -44,17 +42,12 @@ app.get('/favicon.ico', (req, res) => {
 });
 
 // Home page
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 // main page POST
-app.post("/", (req, res) => {
-// testing routePaths
-  //const route_path_not_dupe = helpers.checkForDupe(generatedNum);     WILL WORK ON THIS LATER
-  //console.log('checking route_path_not_dupe: ', route_path_not_dupe);
-
-  //fill me with javascript please for when the creator submits the initial form
+app.post('/', (req, res) => {
   const generatedNum = helpers.generateRandomChars('0123456789abcdefghijklmnopqrstuvwxyz', 6);
 
   helpers.insertIntoPollsTable(req.body, generatedNum)
@@ -62,10 +55,10 @@ app.post("/", (req, res) => {
       const optionArray = req.body.option;
       const descriptionArray = req.body.description;
 
-      optionArray.forEach((value,index) => {
+      optionArray.forEach((value, index) => {
         helpers.fetchOptions()
           .then((option) => {
-            console.log('Option:', value, "Description:", descriptionArray[index]);
+            console.log('Option:', value, 'Description:', descriptionArray[index]);
 
             if (value !== '' ){
               helpers.insertIntoOptionsTable(value, descriptionArray[index], polls[0])
@@ -83,7 +76,7 @@ app.post("/", (req, res) => {
 });
 
 // Poll page
-app.get("/:id", (req, res) => {
+app.get('/:id', (req, res) => {
   let tempId = req.params.id;
   helpers.fetchPollAtRoutePath(tempId)
     .then((polls) => {
@@ -96,13 +89,13 @@ app.get("/:id", (req, res) => {
             pollRoutePath: polls[0].routePath,
             optionsArr: options
           };
-          res.render("poll", templateVars);
+          res.render('poll', templateVars);
         });
     });
 });
 
 // poll page POST
-app.post("/:id", (req, res) => {
+app.post('/:id', (req, res) => {
   //fill me with javascript please for when the user submits poll rankins
   let pollId = req.headers.referer.slice(-6);
   helpers.fetchPollAtRoutePath(pollId)
@@ -110,7 +103,7 @@ app.post("/:id", (req, res) => {
       helpers.fetchOptionsAtPollId(polls[0])
         .then((options) => {
           for (let option in options) {
-            console.log("Title", options[option].option_title, 'ID:', options[option].id, 'Score:', req.body[options[option].option_title]);
+            console.log('Title', options[option].option_title, 'ID:', options[option].id, 'Score:', req.body[options[option].option_title]);
             helpers.insertIntoRatingsTable(req.body, options[option])
               .then();
           }
@@ -120,12 +113,12 @@ app.post("/:id", (req, res) => {
 });
 
 // Poll results page
-app.get("/:id/results", (req, res) => {
+app.get('/:id/results', (req, res) => {
   let pollId = req.params.id;
   // templateVars.options array needs to have something in it to work
   // This placeholder is removed further down
   let templateVars = {
-    options: ["oh"],
+    options: ['oh'],
     pollTitle: '',
     poll_id: pollId
   };
@@ -137,7 +130,7 @@ app.get("/:id/results", (req, res) => {
     // helpers.fetchRatingsAtPollId(pollId)
     .then((queryResults) => {
       queryResults.forEach(result => {
-          // add poll_title and email to templateVars
+        // add poll_title and email to templateVars
         templateVars.pollTitle = result.poll_title;
         templateVars.email = result.email;
 
@@ -153,7 +146,7 @@ app.get("/:id/results", (req, res) => {
         if (isInTemplateVars) {
           // add result rating to option
           templateVars.options[i].ratings.push(result.rating);
-          templateVars.options[i].totalScore += result.rating
+          templateVars.options[i].totalScore += result.rating;
         } else {
           // add option to options array
           templateVars.options.push({
@@ -168,8 +161,8 @@ app.get("/:id/results", (req, res) => {
       });
       // Remove placeholder
       let removeFirst = templateVars.options.shift();
-      //console.log("Options:", templateVars.options);
-      templateVars.options.sort(function(a, b){
+      //console.log('Options:', templateVars.options);
+      templateVars.options.sort((a, b) => {
         if (a.totalScore < b.totalScore) {
           return 1;
         }
@@ -178,11 +171,11 @@ app.get("/:id/results", (req, res) => {
         }
         return 0;
       });
-      //console.log("Options POST SORT:", templateVars.options);
+      //console.log('Options POST SORT:', templateVars.options);
       res.render('results', templateVars);
     });
 });
 
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  console.log('Example app listening on port ' + PORT);
 });
