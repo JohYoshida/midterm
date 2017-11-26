12776bf0715dcb42fd96fa626cea37d713ae70ee
Joh Yoshida <johyoshida@gmail.com>
2017-11-26 14:35:05 -0800

    Linting and conversion to ES5

--- public/scripts/ratings-button.js
+++ public/scripts/ratings-button.js
@@ -1,14 +1,14 @@
 $(() => {
-  $('#options-submit').on('click', event => {
+  $('#options-submit').on('click', function(event) {
     event.preventDefault();
 
-    let $options = $('.options').children().find('h3');
-    let dataObj = makeDataObject($options);
+    var $options = $('.options').children().find('h3');
+    var dataObj = makeDataObject($options);
     $.ajax({
       url: '/:id',
       method: 'POST',
       data: dataObj,
-      success: () => {
+      success: function(){
         // hide form and show thank you
         $('.results-form').slideUp('slow', function(){
           $('.form-cont').append('<div class="alert alert-success thank-you">Thank you for your submission</div>');
@@ -20,11 +20,11 @@ $(() => {
 });
 
 function makeDataObject(options) {
-  let obj = {}
-  for (let title in options) {
-    let rank = Number(title) + 1;
+  var obj = {}
+  for (var title in options) {
+    var rank = Number(title) + 1;
     if (rank) {
-      let key = options[title].innerHTML;
+      var key = options[title].innerHTML;
       obj[key] = options.length - rank + 1;
     }
   }
--- public/scripts/thank-you.js
+++ public/scripts/thank-you.js
@@ -1,10 +1,10 @@
-$(() => {
-  $("#options-submit").on("click", event => {
-    event.preventDefault();
-
-    $('.results-form').slideUp('slow', function(){
-      $('.form-cont').append('<div class="alert alert-success thank-you">Thank you for your submission</div>');
-    });
-  });
-
-});
+// $(() => {
+//   $("#options-submit").on("click", event => {
+//     event.preventDefault();
+//
+//     $('.results-form').slideUp('slow', function(){
+//       $('.form-cont').append('<div class="alert alert-success thank-you">Thank you for your submission</div>');
+//     });
+//   });
+//
+// });
